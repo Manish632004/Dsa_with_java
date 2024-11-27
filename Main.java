a304754a8a766759ffaf99b635f6c4df0dbcac17
Manish Kumar <mainsh632004@gmail.com>
2024-11-27 16:33:18 +0530

    lecture 8

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+       String [] arr ={"I","V","X","L","C","D","M"};
+
+        Scanner in  = new Scanner(System.in);
+        System.out.println();
+
+        String roman = in.next();
+
+        for (int i = 0; i < arr.length; i++) {
+
+        }
+
+
+    }
+}
+
